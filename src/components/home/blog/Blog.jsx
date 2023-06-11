189fc43a606ab7e2641754133f8572c4322074dd
hmaminulislam <mdaminulislamsahin14@gmail.com>
2023-06-11 09:59:19 +0600

    blog section Done

--- src/components/home/blog/Blog.jsx
+++ src/components/home/blog/Blog.jsx
@@ -1,29 +1,36 @@
 import BlogImg1 from "../../../assets/blog/blog-1.jpg";
+import BlogImg2 from "../../../assets/blog/blog-2.jpg";
+import BlogImg3 from "../../../assets/blog/blog-3.jpg";
 import AuthorImg1 from "../../../assets/blog/author-1.png";
+import AuthorImg2 from "../../../assets/blog/author-2.png";
+import AuthorImg3 from "../../../assets/blog/author-3.png";
 import { BsArrowRight } from "react-icons/bs";
 import { FaRegComments } from "react-icons/fa";
+import '../../home/blog/blog.css'
 
 function Blog() {
   return (
     <div className="max-w-[1300px] mx-auto px-5 py-20">
       <div className="mb-10">
         <h4 className="caveat-font text-3xl text-primary">News & Blogs</h4>
-        <h2 className="text-4xl sm:text-5xl font-semibold mt-2">Our Latest Foods Blog</h2>
+        <h2 className="text-4xl sm:text-5xl font-semibold mt-2">
+          Our Latest Foods Blog
+        </h2>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 lg:justify-between sm:justify-start justify-center gap-10">
+      <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 sm:justify-start lg:justify-between gap-y-10 lg:gap-x-10 sm:gap-x-5">
         {/* blog card start */}
-        <div className="border rounded-[5px] max-w-[390px]">
-          <div className="relative">
+        <div className="border rounded-[5px] max-w-[390px] mx-auto sm:mx-0 blog-box">
+          <div className="blog-box-img-wrap">
             <img
-              className="w-full h-[300px] rounded-t-[5px]"
+              className="w-full h-[300px] blog-box-img rounded-t-[5px]"
               src={BlogImg1}
               alt=""
             />
-            <button className="btn-bg-primary text-white py-1 px-4 text-sm absolute right-[20px] bottom-[-8px]">
+          </div>
+          <div className="py-5 px-5 relative">
+            <button className="btn-bg-primary text-white py-1 px-4 text-sm absolute right-[20px] top-[-8px]">
               Restaurant
             </button>
-          </div>
-          <div className="py-5 px-5">
             <div className="flex items-center gap-4 mb-4">
               <img
                 className="w-[60px] h-[60px] rounded-full border-[5px] border-white shadow-md"
@@ -31,12 +38,12 @@ function Blog() {
                 alt=""
               />
               <div>
-                <h5 className="font-[600]">Jhon doe</h5>
-                <p className="text-sm font-[300] text-primary">30 jun, 2023</p>
+                <h5 className="font-[600] text-[18px]">Stephanie</h5>
+                <p className="text-xs font-[300] text-primary">30 Jun, 2023</p>
               </div>
             </div>
-            <h3 className="text-[26px] font-[700]">
-              Introducing Our New Summer Menu!
+            <h3 className="text-[26px] font-[700] blog-box-title">
+              Operates approximately 200 restaurants
             </h3>
             <p className="text-sm font-[300] mt-2">
               Alienum phaedrum torquatos nec eu, vis detraxit periculis ex,
@@ -44,7 +51,7 @@ function Blog() {
             </p>
             <div className="border-b my-4"></div>
             <div className="flex items-center justify-between">
-              <button className="flex items-center gap-3">
+              <button className="blog-box-btn flex items-center gap-3">
                 Read More <BsArrowRight />
               </button>
               <p className="text-primary flex items-center gap-2">
@@ -55,31 +62,31 @@ function Blog() {
         </div>
         {/* blog card end */}
         {/* blog card start */}
-        <div className="border rounded-[5px] max-w-[390px]">
-          <div className="relative">
+        <div className="border rounded-[5px] max-w-[390px] mx-auto sm:mx-0 blog-box">
+          <div className="blog-box-img-wrap">
             <img
-              className="w-full h-[300px] rounded-t-[5px]"
-              src={BlogImg1}
+              className="w-full h-[300px] blog-box-img rounded-t-[5px]"
+              src={BlogImg2}
               alt=""
             />
-            <button className="btn-bg-primary text-white py-1 px-4 text-sm absolute right-[20px] bottom-[-8px]">
+          </div>
+          <div className="py-5 px-5 relative">
+            <button className="btn-bg-primary text-white py-1 px-4 text-sm absolute right-[20px] top-[-8px]">
               Restaurant
             </button>
-          </div>
-          <div className="py-5 px-5">
             <div className="flex items-center gap-4 mb-4">
               <img
                 className="w-[60px] h-[60px] rounded-full border-[5px] border-white shadow-md"
-                src={AuthorImg1}
+                src={AuthorImg2}
                 alt=""
               />
               <div>
-                <h5 className="font-[600]">Jhon doe</h5>
-                <p className="text-sm font-[300] text-primary">30 jun, 2023</p>
+                <h5 className="font-[600] text-[18px]">Gilbert</h5>
+                <p className="text-xs font-[300] text-primary">30 Jun, 2023</p>
               </div>
             </div>
-            <h3 className="text-[26px] font-[700]">
-              Introducing Our New Summer Menu!
+            <h3 className="text-[26px] font-[700] blog-box-title">
+              Tender fried baby squid with a salt, pepper
             </h3>
             <p className="text-sm font-[300] mt-2">
               Alienum phaedrum torquatos nec eu, vis detraxit periculis ex,
@@ -87,7 +94,7 @@ function Blog() {
             </p>
             <div className="border-b my-4"></div>
             <div className="flex items-center justify-between">
-              <button className="flex items-center gap-3">
+              <button className="blog-box-btn flex items-center gap-3">
                 Read More <BsArrowRight />
               </button>
               <p className="text-primary flex items-center gap-2">
@@ -98,30 +105,30 @@ function Blog() {
         </div>
         {/* blog card end */}
         {/* blog card start */}
-        <div className="border rounded-[5px] max-w-[390px]">
-          <div className="relative">
+        <div className="border rounded-[5px] max-w-[390px] mx-auto sm:mx-0 blog-box">
+          <div className="blog-box-img-wrap">
             <img
-              className="w-full h-[300px] rounded-t-[5px]"
-              src={BlogImg1}
+              className="w-full h-[300px] blog-box-img rounded-t-[5px]"
+              src={BlogImg3}
               alt=""
             />
-            <button className="btn-bg-primary text-white py-1 px-4 text-sm absolute right-[20px] bottom-[-8px]">
+          </div>
+          <div className="py-5 px-5 relative">
+            <button className="btn-bg-primary text-white py-1 px-4 text-sm absolute right-[20px] top-[-8px]">
               Restaurant
             </button>
-          </div>
-          <div className="py-5 px-5">
             <div className="flex items-center gap-4 mb-4">
               <img
                 className="w-[60px] h-[60px] rounded-full border-[5px] border-white shadow-md"
-                src={AuthorImg1}
+                src={AuthorImg3}
                 alt=""
               />
               <div>
-                <h5 className="font-[600]">Jhon doe</h5>
-                <p className="text-sm font-[300] text-primary">30 jun, 2023</p>
+                <h5 className="font-[600] text-[18px]">Taniya</h5>
+                <p className="text-xs font-[300] text-primary">30 Jun, 2023</p>
               </div>
             </div>
-            <h3 className="text-[26px] font-[700]">
+            <h3 className="text-[26px] font-[700] blog-box-title">
               Introducing Our New Summer Menu!
             </h3>
             <p className="text-sm font-[300] mt-2">
@@ -130,7 +137,7 @@ function Blog() {
             </p>
             <div className="border-b my-4"></div>
             <div className="flex items-center justify-between">
-              <button className="flex items-center gap-3">
+              <button className="blog-box-btn flex items-center gap-3">
                 Read More <BsArrowRight />
               </button>
               <p className="text-primary flex items-center gap-2">
