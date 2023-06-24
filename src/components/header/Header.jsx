b063259ac31b8486f5ea9f4603e84f4b85193eeb
hmaminulislam <mdaminulislamsahin14@gmail.com>
2023-06-24 14:08:15 +0600

    navbar modification

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -45,12 +45,12 @@ export default function Header() {
     // disabled pased hour
 
   return (
-    <div className="bg-white px-5 relative">
+    <div className="bg-white px-5">
       <div className="max-w-[1300px] mx-auto md:h-[100px] md:flex items-center justify-between overflow-hidden">
         <div className="flex md:flex-none items-center justify-between h-[60px] md:h-auto">
           <div>
             <Link to="/">
-              <img className="w-[120px]" src={Logo} alt="" />
+              <img className="w-[120px] z-[999]" src={Logo} alt="" />
             </Link>
           </div>
           <div className="md:hidden">
@@ -62,13 +62,9 @@ export default function Header() {
             ></VscMenu>
           </div>
         </div>
-        <div
-          className={`bg-white md:bg-transparent absolute md:static w-full md:w-auto left-0 px-5 py-4 ${
-            !show ? "block z-10" : "hidden md:block"
-          }`}
-        >
-          <div className="md:flex items-center gap-10">
-            <ul className="md:flex md:gap-8">
+        <div className={`navbar ${!show ? "show-nav" : "hidden-nav"} z-[999]`}>
+          <div className="md:flex items-center justify-end gap-10 w-full">
+            <ul className="md:flex md:gap-8 w-full md:w-auto">
               <li className="font-semibold my-3 md:my-0 border-b border-gray-200 md:border-0 pb-2 md:pb-0 hover:text-[#FE5D26] transition duration-300">
                 <NavLink to="/">Home</NavLink>
               </li>
@@ -94,6 +90,7 @@ export default function Header() {
           </div>
         </div>
       </div>
+
       {/* Book Popup  */}
 
       <dialog id="BookPopUp" className="modal">
@@ -115,7 +112,7 @@ export default function Header() {
               ONLINE RESERVATION
             </p>
             <h3 className="text-4xl text-center mt-4 mb-6">BOOK A TABLE</h3>
-            <form className='text-center'>
+            <form className="text-center">
               <select
                 name="time"
                 id="time"
@@ -132,8 +129,13 @@ export default function Header() {
                 <option value="9 person">9 Person</option>
                 <option value="10 person">10 Person</option>
               </select>
-              <div className='mt-3'>
-              <input type="date" min={minDate} max={maxDate} className="input input-bordered w-full max-w-xs date" />
+              <div className="mt-3">
+                <input
+                  type="date"
+                  min={minDate}
+                  max={maxDate}
+                  className="input input-bordered w-full max-w-xs date"
+                />
               </div>
               <select
                 name="time"
@@ -157,7 +159,9 @@ export default function Header() {
                 <option value="10:00 pm">10:00 pm</option>
                 <option value="11:00 pm">11:00 pm</option>
               </select>
-              <button className='btn-bg-primary w-full max-w-xs text-white py-3 mt-4 rounded'>BOOK NOW</button>
+              <button className="btn-bg-primary w-full max-w-xs text-white py-3 mt-4 rounded">
+                BOOK NOW
+              </button>
             </form>
           </div>
         </form>
