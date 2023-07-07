0a16f77db3d56f158db0fa4c353bf401f6dacb26
hmaminulislam <mdaminulislamsahin14@gmail.com>
2023-07-07 08:47:44 +0600

    blog details modify

--- src/components/blog/BlogDetails.jsx
+++ src/components/blog/BlogDetails.jsx
@@ -1,12 +1,20 @@
 import { useLoaderData } from "react-router-dom"
+import './blog-details.css'
 
 
 function BlogDetails() {
   const data = useLoaderData()
   console.log(data)
   return (
-    <div>BlogDetails</div>
-  )
+    <div>
+      <div className="blog-details-hero sm:h-[300px] h-[180px] flex justify-center items-center">
+        <h2 className="text-white sm:text-5xl text-4xl font-semibold">
+          {data.name}
+        </h2>
+      </div>
+      <div className="max-w-[1300px] mx-auto px-5 py-20"></div>
+    </div>
+  );
 }
 
 export default BlogDetails
